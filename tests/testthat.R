ba082165a3432c810e538d7d5633cd375baa0c3f
jorainer <johannes.rainer@gmail.com>
2021-11-25 12:34:33 +0100

    tests: run test suite from Spectra
    - Run the test suite for spectra variables from the `Spectra` package.

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,18 @@ library(testthat)
 library(MsBackendMgf)
 
 test_check("MsBackendMgf")
+
+## Run tests defined in test suites from the Spectra package.
+fls <- dir(system.file("extdata", package = "MsBackendMgf"),
+           full.names = TRUE, pattern = "mgf$")[1:2]
+be <- MsBackendMgf()
+be <- backendInitialize(be, fls)
+
+## Run the MsBackend spectra variable test suite
+test_suite <- system.file("test_backends", "test_MsBackend",
+                          package = "Spectra")
+
+## Run single test file.
+test_file(paste0(test_suite, "/test_spectra_variables.R"))
+## Run the whole suite.
+## test_dir(test_suite)
